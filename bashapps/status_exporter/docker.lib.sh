14af2b674df45d4bd41500ea56b0ba288f352116
Blase-ssa <61013982+Blase-ssa@users.noreply.github.com>
2024-12-25 13:23:51 +0100

    feature: status exporter create docker library start work on server exporter

--- bashapps/status_exporter/docker.lib.sh
+++ bashapps/status_exporter/docker.lib.sh
@@ -2,3 +2,21 @@
 # Set of functions for getting docker metrics in json format
 # Require: Docker 
 
+docker_check_requirement(){
+    ### check if the Docker installed in the system
+    if command -v tmux > /dev/null 2>&1; then
+        return true
+    else
+        return false
+    fi
+}
+
+docker_get_containers(){
+    ### get list of container(s) and they're status in json format
+    return $(docker ps -a --format json)
+}
+
+docker_get_resource_usage(){
+    ### get container(s) resource usage statistics in json format
+    return $(docker stats -a --format json --no-stream)
+}
--- bashapps/status_exporter/pc.lib.sh
+++ bashapps/status_exporter/pc.lib.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env bash
+# Set of functions for getting local server metrics in json format
+# Require: awk, jq
+
+pc_get_mem(){
+    pc_mem_r=$(free -b | awk 'NR>1 {print "{\""$1"\":{\"Total\":\""$2"\",\"Used\":\""$3"\",\"Free\":\""$4"\",\"Shared\":\""$5"\",\"Cached\":\""$6"\",\"Available\":\""$7"\"}}"}' | jq | jq 'del(.["Swap:"].Shared, .["Swap:"].Cached, .["Swap:"].Available)')
+    pc_swap_r=$(swapon --show --bytes| awk 'NR>1 {print "{\"Device\":\""$1"\",\"Type\":\""$2"\",\"Size\":\""$3"\",\"Used\":\""$4"\",\"Priority\":\""$5"\"}"}')
+    return $(echo "$pc_mem_r" | jq --argjson details "$pc_swap_r" '.["Swap:"].Details = $details')
+}
+
+pc_get_cpu(){
+
+}
+
+pc_get_storage(){
+    return $(df --exclude-type=tmpfs -h| awk 'NR>1 {print "{\"Filesystem\":\""$1"\",\"Size\":\""$2"\",\"Used\":\""$3"\",\"Avail\":\""$4"\",\"Use\":\""$5"\",\"Mountpoint\":\""$6"\"}"}' | jq -s .)
+}
